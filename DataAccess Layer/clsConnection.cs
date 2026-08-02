9b21d16ede67111ea73ba93d05eb6f3d76ae4661
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-08-02 20:56:09 +0100

    #2
    Make clsConnection public and use config for connection string
    
    Changed clsConnection access modifier to public for external use.
    Replaced hardcoded connection string with dynamic retrieval from
    the configuration file via ConfigurationManager. Added using
    directive for System.Configuration.

--- DataAccess Layer/clsConnection.cs
+++ DataAccess Layer/clsConnection.cs
@@ -4,11 +4,11 @@ using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
 namespace DataAccess_Layer
 {
-    internal class clsConnection
+    public class clsConnection
     {
-        public static string DBConnectionString = "Server=.;Database = CarWachMS;User Id=sa;Password=NewStrongPassword123!";
+        public static string DBConnectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString  ;
     }
 }
